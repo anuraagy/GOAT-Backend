94c71efa739cfd7ec00f489db8a78b3d7c7378fe
Anuraag Yachamaneni <ayachama@purdue.edu>
2017-01-24 14:03:58 -0500

    CORE-4: Finish tests for exercise type

--- app/controllers/api/v1/exercise_types_controller.rb
+++ app/controllers/api/v1/exercise_types_controller.rb
@@ -14,8 +14,18 @@ class Api::V1::ExerciseTypesController < Api::V1::BaseController
     end
   end
 
+  def update
+    exercise_type = ExerciseType.find(params[:id])
+
+    if exercise_type.update(exercise_params)
+      render :json => exercise_type.as_json
+    else
+      render :json => exercise_type.errors.as_json
+    end
+  end
+
   def search
-    exercise_types = ExerciseType.where(food_params)
+    exercise_types = ExerciseType.where(exercise_params)
     render :json => exercise_types.as_json
   end
 
